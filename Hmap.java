b876cbd681e650068a40feb8a45ac69d92d4cb80
stardom98 <myselfsbm@gmail.com>
2021-09-17 16:53:12 +0530

    Changed Format

--- Hmap.java
+++ Hmap.java
@@ -1,18 +1,18 @@
 import java.util.HashMap;
 
 public class Hmap {
-	public static void main(String[] args) {
-	
-	HashMap<String, String> cars = new HashMap<String, String>();
-	
-	cars.put("Maruti","Swift");
-	cars.put("Ford","Mustang");
-	cars.put("Toyota","Innova");
-	cars.put("Hyundai","i20");
-	cars.put("Renault","Duster");
-	
-	for(String i : cars.keySet())  {
-		System.out.println("Brand: "+ i +"  Model: "+ cars.get(i));
-		}
-	}
+    public static void main(String[] args) {
+
+        HashMap < String, String > cars = new HashMap < String, String > ();
+
+        cars.put("Maruti", "Swift");
+        cars.put("Ford", "Mustang");
+        cars.put("Toyota", "Innova");
+        cars.put("Hyundai", "i20");
+        cars.put("Renault", "Duster");
+
+        for (String i: cars.keySet()) {
+            System.out.println("Brand: " + i + "  Model: " + cars.get(i));
+        }
+    }
 }
--- Hset.java
+++ Hset.java
@@ -1,17 +1,17 @@
 import java.util.HashSet;
 
 public class Hset {
-	public static void main(String[] args) {
-		HashSet<String> set = new HashSet<String>();
-			set.add("Burger");
-			set.add("Pizza");
-			set.add("Roti");
-			set.add("Panini");
-			set.add("Pizza");
-			set.add("frankie");
-			
-			for(String i : set) {
-				System.out.println(i);
-			}
-		}
-	}
+    public static void main(String[] args) {
+        HashSet < String > set = new HashSet < String > ();
+        set.add("Burger");
+        set.add("Pizza");
+        set.add("Roti");
+        set.add("Panini");
+        set.add("Pizza");
+        set.add("frankie");
+
+        for (String i: set) {
+            System.out.println(i);
+        }
+    }
+}
